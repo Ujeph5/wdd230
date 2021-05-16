37ef907d12371f2e92ada0771badb2489bd1bdd1
Ujeph5 <j.s.alexander.v@gmail.com>
2021-05-15 21:37:59 -0300

    Southeast Idaho Weather site

---
+++ lesson4/js/index.js
@@ -0,0 +1,11 @@
+let d = new Date();
+document.querySelector("#year").innerHTML = d.getFullYear();
+
+const menuButton = document.querySelector(".menu");
+const mainnav = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {mainnav.classList.toggle("responsive"); }, false);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
