75c5737aad6bd6755072a252f02f5bb9b5251f86
Ujeph5 <j.s.alexander.v@gmail.com>
2021-06-16 19:43:46 -0300

    gallery page

--- lesson7/js/index.js
+++ lesson7/js/index.js
@@ -16,3 +16,23 @@ let day = d.getDay();
 if (day != 5) {
   document.querySelector(".banner").style.display = "none";
 }
+
+// get number of days since last visit and display to screen
+function getNumberOfDays() {
+  const date1 = localStorage.lastDate;
+  const date2 = new Date();
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  document.querySelector("#daysSinceLastVisit").textContent = diffInDays;
+}
+
+// save current date in local storage
+function saveDate() {
+  let lastDate = new Date();
+  localStorage.lastDate = lastDate;
+}
