cb9b519a726f3b9739b5ff965a6965e77a812c2e
Ujeph5 <j.s.alexander.v@gmail.com>
2021-06-17 20:41:34 -0300

    updated lesson8

---
+++ lesson8/js/index.js
@@ -0,0 +1,44 @@
+let d = new Date();
+document.querySelector("#year").innerHTML = d.getFullYear();
+
+// hamburger menu
+const menuButton = document.querySelector(".menu");
+const mainnav = document.querySelector(".navigation");
+
+menuButton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+// get number of days since last visit and display to screen
+function getNumberOfDays() {
+  const date1 = localStorage.lastDate;
+  const date2 = new Date();
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  document.querySelector("#daysSinceLastVisit").textContent = diffInDays;
+}
+
+// save current date in local storage
+function saveDate() {
+  let lastDate = new Date();
+  localStorage.lastDate = lastDate;
+}
+
+// create banner for fridays only
+let day = d.getDay();
+if (day != 5) {
+  document.querySelector(".banner").style.display = "none";
+}
